f7acf1b7ca2b1d4197ee3dd38bf9f19f85b5ebda
Diana Soltani <dianasoltani@microsoft.com>
2023-05-11 14:46:30 -0700

    Add project files.

---
+++ XamarinWithUWP/XamarinWithUWP/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinWithUWP.Services;
+using XamarinWithUWP.Views;
+
+namespace XamarinWithUWP
+{
+	public partial class App : Application
+	{
+
+		public App()
+		{
+			InitializeComponent();
+
+			DependencyService.Register<MockDataStore>();
+			MainPage = new AppShell();
+		}
+
+		protected override void OnStart()
+		{
+		}
+
+		protected override void OnSleep()
+		{
+		}
+
+		protected override void OnResume()
+		{
+		}
+	}
+}
---
+++ XamarinWithUWP/XamarinWithUWP/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using XamarinWithUWP.Models;
+using XamarinWithUWP.ViewModels;
+
+namespace XamarinWithUWP.Views
+{
+	public partial class NewItemPage : ContentPage
+	{
+		public Item Item { get; set; }
+
+		public NewItemPage()
+		{
+			InitializeComponent();
+			BindingContext = new NewItemViewModel();
+		}
+	}
+}
