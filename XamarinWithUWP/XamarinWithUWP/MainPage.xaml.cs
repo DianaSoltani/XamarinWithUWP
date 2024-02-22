edaa9ecee54ac8e5009274666d728a3c1e037996
Diana Soltani <dianasoltani@microsoft.com>
2024-02-21 17:46:28 -0800

    a

---
+++ XamarinWithUWP/XamarinWithUWP/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace XamarinWithUWP.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            LoadApplication(new XamarinWithUWP.App());
+        }
+    }
+}
